e67a2775dc60ee9ecc5bd1cd1565156b732abe78
Gennady Telegin <gtelegin@gmail.com>
2014-12-17 21:26:51 +0300

    formatting and phpdoc

--- src/LangDetector.php
+++ src/LangDetector.php
@@ -1,6 +1,5 @@
 <?php
 namespace LangDetector;
-
 /*
 *******************************************************************************
 *    This program is free software: you can redistribute it and/or modify
@@ -16,7 +15,9 @@ namespace LangDetector;
 *    You should have received a copy of the GNU General Public License
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************
-*
+*/
+
+/**
 * This class detect the language of a given text.
 * Requires pspell and an aspell dictionary for each language that you want to detect.
 *
@@ -25,90 +26,96 @@ namespace LangDetector;
 */
 class LangDetector
 {
-	private $langs;
-	private $badChars = ',.;:?!#()[]{}<>+-_&@*\'"^\\/%$€£0123456789|';
-	private $thres = 0.75;
+    private $langs;
+    private $badChars = ',.;:?!#()[]{}<>+-_&@*\'"^\\/%$€£0123456789|';
+    private $thres = 0.75;
 
-	/**
-	 * $langs is an array of language codes, e.g. ['it','en','fr']
-	 */
-	public function __construct($langs)
-	{
-		$this->langs = $langs;
-	}
-
-	protected function getLanguages()
-	{
-	    return $this->langs;
-	}
+    /**
+     * @param array $langs array of language codes to detect, e.g. ['it','en','fr']
+     */
+    public function __construct($langs)
+    {
+        $this->langs = $langs;
+    }
 	
-	protected function filterText($text)
-	{
-	    $text = preg_replace('/@\w+/', '', $text); // Filter out instagram-like user references (aka, @mybestfriend)
-	    
-	    $text = strtr($text, $this->badChars, ' ');
-	    $text = preg_replace('/\s+/', ' ', $text);
-	    
-	    return $text;
-	}
-	
-	/**
-	 * Returns an associative array that map each language code to the probability that $text is of that language.
-	 */
-	public function getProbabilities($text)
-	{
-		$probs = array();
+    /**
+     * Returns an associative array that map each language code to the probability that $text is of that language.
+     * 
+     * @param string $text Input text
+     */
+    public function getProbabilities($text)
+    {
+        $probs = array();
 
-		$words = explode(' ', $this->filterText($text));
+        $words = explode(' ', $this->filterText($text));
 
-		$totalWords = count($words);
+        $totalWords = count($words);
 
-		if ($totalWords > 0) {
-    		foreach($this->getLanguages() as $lang) {
-    			$pspell = pspell_new($lang);
-    			$goodWords = 0;
-    			
-    			foreach ($words as $word) {
-    				if (pspell_check($pspell, $word)) {
-    					++$goodWords;
-    				}
-    			}
-    			
-    			$probs[$lang] = $goodWords/$totalWords;
-    		}
+        if ($totalWords > 0) {
+            foreach($this->getLanguages() as $lang) {
+                $pspell = pspell_new($lang);
+                $goodWords = 0;
+
+                foreach ($words as $word) {
+                    if (pspell_check($pspell, $word)) {
+                        ++$goodWords;
+                    }
+                }
+
+                $probs[$lang] = $goodWords/$totalWords;
+            }
+
+            arsort($probs);
+        }
+
+        return $probs;
+    }
+
+    /**
+     * Returns the most probable language for the given $text if the probability is above a threshold (0.75 by default), false otherwhise.
+     * 
+     * @param string $text Input text
+     */
+    public function getLang($text)
+    {
+        $probs = $this->getProbabilities($text);
+        $lang = key($probs);
+
+        if ($probs[$lang] >= $this->thres) {
+            next($probs);
+            $nextLang = key($probs);
+            if ($probs[$nextLang] >= $this->thres) {
+                return false;
+            }
+
+            return $lang;
+        } else {
+                return false;
+        }
+    }
+
+    /**
+     * Set the threshold used by the getLang function.
+     * 
+     * @params float @thres
+     */
+    public function setThres($thres)
+    {
+        $this->thres = $thres;
+    }
     
-    		arsort($probs);
-		}
-		
-		return $probs;
-	}
+    protected function getLanguages()
+    {
+        return $this->langs;
+    }
 
-	/**
-	 * Returns the most probable language for the given $text if the probability is above a threshold (0.75 by default), false otherwhise.
-	 */
-	public function getLang($text)
-	{
-		$probs = $this->getProbabilities($text);
-		$lang = key($probs);
-		
-		if ($probs[$lang] >= $this->thres) {
-		    next($probs);
-		    $nextLang = key($probs);
-		    if ($probs[$nextLang] >= $this->thres) {
-		        return false;
-		    }
-		    
-			return $lang;
-		} else {
-			return false;
-		}
-	}
+    protected function filterText($text)
+    {
+        $text = preg_replace('/@\w+/', '', $text); // Filter out instagram-like user references (aka, @mybestfriend)
 
-	/**
-	 * Set the threshold used by the getLang function.
-	 */
-	public function setThres($thres)
-	{
-		$this->thres = $thres;
-	}
+        $text = strtr($text, $this->badChars, ' ');
+        $text = preg_replace('/\s+/', ' ', $text);
+
+        return $text;
+    }
 }
